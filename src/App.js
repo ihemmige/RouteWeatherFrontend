9b90fe89e17514be658290835392de1559d98348
Ishan Hemmige <ishan.hemmige@gmail.com>
2023-07-18 22:51:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { format } from "react-string-format";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [locations, setLocations] = useState([{}]);
+  const [displaying, setDisplaying] = useState(false);
+  const [fetching, setFetching] = useState(false);
+  useEffect(() => {
+    setDisplaying(false);
+    setFetching(false);
+    setLocations([{}]);
+  }, []);
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    setFetching(true);
+    getRouteWeather(data.origin, data.destination);
+  };
+
+  const getRouteWeather = async (origin, destination) => {
+    let url = format(
+      "https://route-weather-app.onrender.com/?origin={0}&destination={1}",
+      origin,
+      destination
+    );
+    // todo - handle errors from get request
+    const response = await axios.get(url);
+    setLocations(response.data);
+    setDisplaying(true);
+    setFetching(false);
+    reset();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <form class="form" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input
+              type="text"
+              placeholder="Origin"
+              name="origin"
+              class="text-box"
+              {...register("origin", { required: true })}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Destination"
+              name="dest"
+              class = "text-box"
+              {...register("destination", { required: true })}
+            />
+          </div>
+          
+          <input class="submit" type="submit"/>
+          
+        </form>
+      }
+      
+
+      {fetching && <p>Building route and weather data...</p>}
+
+      {displaying && !fetching &&
+        locations.result.map((city, i) => (
+          <div>
+            <p className="App-Link" key={i}>
+              {i + 1}. {city["city"]}, {city["zip_code"]}: {city["weather"]}
+            </p>
+            {/* <p className="App-Link" key={2 * i + 1}>
+              
+            </p> */}
+          </div>
+        ))}
+
+      {!displaying && !fetching && (
+        <p>Provide an  and destination to get weather along the route.</p>
+      )}
     </div>
+    
   );
 }
 
